501dc63d460fd44e34876e6ac56320500eea4ec5
RAHUL GUPTA <42072042+rahul8497@users.noreply.github.com>
2022-04-11 11:23:48 +0530

    Add files via upload
    This is a StopWatchTimer

---
+++ index.js
@@ -0,0 +1,39 @@
+var tens=00;
+var seconds=00;
+var Appendtens=document.getElementById("tens");
+var Appendseconds=document.getElementById("seconds");
+var Startbtn=document.getElementById("Start-button");
+var Stopbtn=document.getElementById("Stop-button");
+var Resetbtn=document.getElementById("Reset-button");
+var interval;
+function StartInterval(){
+    tens++;
+    if(tens<=9){
+        Appendtens.innerHTML="0"+tens;
+    }
+    if(tens>9){
+        Appendtens.innerHTML=tens;
+    }
+    if(tens>99){
+        seconds++;
+        Appendseconds.innerHTML="0"+seconds;
+        tens=0;
+        Appendtens.innerHTML="00";
+    }
+    if(seconds>9){
+        Appendseconds.innerHTML=seconds;
+    }
+}
+Startbtn.onclick=function(){
+    interval=setInterval(StartInterval);
+}
+Stopbtn.onclick=function(){
+    clearInterval(interval);
+}
+Resetbtn.onclick=function(){
+    clearInterval(interval);
+    tens="00";
+    seconds="00";
+    Appendseconds.innerHTML=seconds;
+    Appendtens.innerHTML=tens;
+}
